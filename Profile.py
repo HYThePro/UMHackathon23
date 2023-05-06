da88d91944dfff9963f0e15cfba4a53c8799fb33
HYThePro <130675454+HYThePro@users.noreply.github.com>
2023-05-06 09:35:58 +0800

    Update Profile.py

--- Profile.py
+++ Profile.py
@@ -22,7 +22,7 @@ def run_website():
         input = st.selectbox(label='Name of company', options=data['name_c'])
         count = 0
         for value in data['name_c']:
-            if input == values:
+            if input == value:
                 row = data.iloc[count]
                 st.write("Name of company: " , value)
                 st.write("Incorporated date: ", data[count]['incorporated_date_c'])
