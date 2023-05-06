e5476b7850affce582d3e2fc4e2e3e056ef5ba84
HYThePro <130675454+HYThePro@users.noreply.github.com>
2023-05-06 10:33:59 +0800

    Update Profile.py

--- Profile.py
+++ Profile.py
@@ -39,10 +39,8 @@ def run_website():
                 st.write("Categories: {}, {}, {}, {}, {}, {}, {}, {}".format(row[18], row[19], row[20], row[21], row[23], row[24], row[25], row[26]))
                 chart_data1 = pd.DataFrame([[row[2], row[5], row[8], row[9], row[10], row[11]]], columns=["Total Funding", "Revenue", "Revenue growth", "EBIT", "Employee Growth (6m)", "Employee Growth (12m)"])
                 chart_data2 = pd.DataFrame([[row[8], row[10], row[11]]], columns=["Revenue growth", "Employee Growth (6m)", "Employee Growth (12m)"])
-                chart_data1.plot(kind='bar')
-                plt.show()
-                chart_data2.plot(kind='bar')
-                plt.show()
+                st.pyplot(chart_data1.plot(kind='bar'))
+                
             count = count+1
             
 
